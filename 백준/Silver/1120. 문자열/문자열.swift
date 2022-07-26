73ea9d31a308ce02947e3bb5c1edec04f08b0abc
did250 <92119231+did250@users.noreply.github.com>
2022-07-26 23:49:46 +0900

    [Silver IV] Title: 문자열, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/1120. 문자열/문자열.swift
+++ 백준/Silver/1120. 문자열/문자열.swift
@@ -1,6 +1,5 @@
 var input = readLine()!.split(separator: " ")
 
-var count = Array(repeating: 0, count: 100)
 var t = 100
 var S1 = [Character]()
 var S2 = [Character]()
@@ -11,13 +10,14 @@ for i in input[1]{
     S2.append(i)
 }
 for i in 0...S2.count-S1.count{
+    var s = 0
     for j in 0...S1.count-1{
         if S1[j] != S2[j+i]{
-            count[i] += 1
+            s += 1
         }
     }
-    if ( t>count[i]){
-        t = count[i]
+    if ( t>s){
+        t = s
     }
 }
 print(t)
