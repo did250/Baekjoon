8da9a28b37945fdd6989686810245f1f13ad2f3c
did250 <92119231+did250@users.noreply.github.com>
2022-07-28 15:43:01 +0900

    [Silver II] Title: 마인크래프트, Time: 228 ms, Memory: 71116 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.swift
@@ -0,0 +1,73 @@
+var input = readLine()!.split(separator: " ").map { Int(String($0))!}
+
+var N = input[0]
+var M = input[1]
+var B = input[2]
+var arr = Array(repeating: Array(repeating: 0, count: M), count: N)
+var min = 256
+var max = 0
+var total = B
+for i in 0...N-1{
+    var temp = readLine()!.split(separator: " ").map { Int(String($0))!}
+    for j in 0...M-1{
+        arr[i][j] = temp[j]
+        if arr[i][j]<min{
+            min = arr[i][j]
+        }
+        if arr[i][j]>max{
+            max = arr[i][j]
+        }
+        total += arr[i][j]
+    }
+}
+var an = [Int]()
+var an2 = [Int]()
+var number = N*M
+var need = 0
+var answer = Int()
+for s in min...max{
+    
+    var blocks = B
+    answer = 0
+    var cnt = 0
+    if total >= s*N*M{
+        loop : for i in 0...N-1{
+            for j in 0...M-1{
+                if arr[i][j] == s {
+                    
+                }
+                else if arr[i][j] > s{
+                    var t = arr[i][j]-s
+                    answer += 2*t
+                    
+                }
+                else {
+                    answer += s-arr[i][j]
+                }
+            }
+            if i == N-1{
+                an.append(answer)
+                an2.append(s)
+                
+            }
+        }
+    }
+    
+}
+
+var ans = an[0]
+var ans2 = 0
+for i in 0...an.count-1{
+    
+    if an[i]<ans{
+        ans=an[i]
+        ans2 = an2[i]
+    }
+    else if an[i] == ans{
+        if an2[i]>ans2 {
+            ans2 = an2[i]
+        }
+    }
+}
+print(ans,terminator: " ")
+print(ans2)
