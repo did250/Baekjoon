d5bd94d8b20525d650715ac6cbd2f0804e65cedc
did250 <92119231+did250@users.noreply.github.com>
2022-08-19 00:54:39 +0900

    [Gold V] Title: 동전 1, Time: 20 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.swift
+++ 백준/Gold/2293. 동전 1/동전 1.swift
@@ -8,8 +8,8 @@ for _ in 1...input[0] {
 var dp = Array(repeating: 0, count: input[1]+1)
 dp[0] = 1
 for i in 0...input[0]-1 {
-    for j in 1...input[1] {
-        if j >= coins[i] {
+    if coins[i] <= input[1] {
+        for j in coins[i]...input[1] {
             if dp[j] + dp[j - coins[i]] < Int(pow(2.0, 31.0)){
                 dp[j] += dp[j-coins[i]]
             }
