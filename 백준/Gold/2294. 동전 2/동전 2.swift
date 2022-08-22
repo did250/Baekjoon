230b5f1810afd60b725911f637895225db444b88
did250 <92119231+did250@users.noreply.github.com>
2022-08-22 22:35:07 +0900

    [Gold V] Title: 동전 2, Time: 32 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.swift
@@ -0,0 +1,23 @@
+let input = readLine()!.split(separator: " ").map {Int(String($0))!}
+let (n,k) = (input[0], input[1])
+var arr = [Int]()
+var dp = Array(repeating: 10001, count: k+1)
+for _ in 1...n {
+    let input = Int(readLine()!)!
+    if !arr.contains(input) && input <= k{
+        arr.append(input)
+    }
+}
+arr.sort()
+dp[0] = 0
+for i in 0...arr.count-1 {
+    for j in arr[i]...k {
+        dp[j] = min(dp[j], dp[j-arr[i]] + 1)
+    }
+}
+if dp[k] == 10001 {
+    print("-1")
+}
+else {
+    print(dp[k])
+}
