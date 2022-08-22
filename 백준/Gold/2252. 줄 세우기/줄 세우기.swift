9b2ca3aea2ed4502ed0dbf81fb0d6a0054f91ea6
did250 <92119231+did250@users.noreply.github.com>
2022-08-22 21:09:52 +0900

    [Gold III] Title: 줄 세우기, Time: 220 ms, Memory: 82552 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+let input = readLine()!.split(separator: " ").map({Int(String($0))!})
+let N = input[0]
+let M = input[1]
+var lower = Array(repeating: [Int](), count: N+1)
+var degree = Array(repeating: 0, count: N+1)
+
+for _ in 0..<M{
+    let input = readLine()!.split(separator: " ").map({Int(String($0))!})
+    let A = input[0]
+    let B = input[1]
+    lower[B].append(A)
+    degree[A] += 1
+}
+
+var queue = [Int]()
+for i in 1..<N+1{
+    if degree[i] == 0{
+        queue.append(i)
+    }
+}
+
+var index = 0
+var answer = [Int]()
+
+while !queue.isEmpty{
+    let cur = queue.removeFirst()
+    answer.append(cur)
+    
+    for i in lower[cur]{
+        degree[i] -= 1
+        if degree[i] == 0{
+            queue.append(i)
+        }
+    }
+    index += 1
+}
+
+answer.reverse()
+for i in answer{
+    print(i, terminator: " ")
+}
