9a625870398045b03bf41d143eda49a48911fb14
did250 <92119231+did250@users.noreply.github.com>
2022-09-09 20:10:52 +0900

    [Gold V] Title: 최소비용 구하기, Time: 312 ms, Memory: 76084 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.swift
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.swift
@@ -1,15 +1,13 @@
 let N = Int(readLine()!)!
 let M = Int(readLine()!)!
+
 var arr : [[Int]] = Array(repeating: [], count: M)
 var visited : [Bool] = Array(repeating: false, count: N+1)
 var map = Array(repeating: 100000001,count: N+1)
-var graph : [[Int]] = Array(repeating: [], count: N+1)
 
 for i in 0...M-1{
     arr[i] = readLine()!.split(separator: " ").map{Int(String($0))!}
-    graph[arr[i][0]].append(arr[i][1])
 }
-
 
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (s,e) = (input[0],input[1])
@@ -27,33 +25,27 @@ for i in 0...M-1 {
     }
 }
 visited[s] = true
-solve()
-
-func solve() {
-    var next = s
-    while (true) {
-        var m = 100000001
-        var cur = 0
-        for i in 1...N {
-            if !visited[i] && m > map[i] {
-                m = map[i]
-                cur = i
-            }
+var next = s
+while (true) {
+    var m = 100000001
+    var cur = 0
+    for i in 1...N {
+        if !visited[i] && m > map[i] {
+            m = map[i]
+            cur = i
         }
-        next = cur
-        visited[next] = true
-        
-        
-        if m == 100000001 {
-            break
+    }
+    next = cur
+    visited[next] = true
+    if m == 100000001 {
+        break
+    }
+    for i in 0...M-1 {
+        if arr[i][0] == next {
+            map[arr[i][1]] = min(map[arr[i][1]], arr[i][2] + map[arr[i][0]])
         }
-        for i in 0...M-1 {
-            if arr[i][0] == next {
-                map[arr[i][1]] = min(map[arr[i][1]], arr[i][2] + map[arr[i][0]])
-            }
-        }
-    
     }
 }
+
 print(map[e])
 
