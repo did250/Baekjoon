83ff42f0d1adbaca569abf3dd8af29b4b403dcb9
did250 <92119231+did250@users.noreply.github.com>
2022-09-13 03:45:23 +0900

    [Platinum IV] Title: 컨닝, Time: 28 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Platinum/1014. 컨닝/컨닝.swift
+++ 백준/Platinum/1014. 컨닝/컨닝.swift
@@ -1,19 +1,15 @@
-import Foundation
-let file = FileIO()
-let T = file.readInt()
-var N = 0
-var M = 0
+let T = Int(readLine()!)!
+var (N, M) = (0,0)
 for _ in 0..<T {
     
-//    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
-//    (N,M) = (input[0],input[1])
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    (N, M) = (input[0],input[1])
     var answer = 0
-    N = file.readInt()
-    M = file.readInt()
+    
     var dp = Array(repeating: Array(repeating: -1, count: (1 << M) ), count: N+1)
     var map : [[String]] = Array(repeating: [], count: N)
     for i in 0...N-1 {
-        map[i] = file.readString().map{String($0)}
+        map[i] = readLine()!.map{String($0)}
     }
     var canbe = [Int]()
     var count = [Int]()
@@ -24,8 +20,9 @@ for _ in 0..<T {
         }
     }
     var index = 0
+    var p = blocked(map[0])
     for i in canbe {
-        if blocked(map[0], i) {
+        if i | p - p == i {
             dp[0][i] = count[index]
             answer = max(answer, dp[0][i])
         }
@@ -34,8 +31,9 @@ for _ in 0..<T {
     if N != 1 {
         for i in 1...N-1 {
             index = 0
+            var t = blocked(map[i])
             for j in canbe {
-                if blocked(map[i], j) {
+                if j | t - t == j {
                     for s in canbe {
                         if can(s, j) {
                             dp[i][j] = max(dp[i][j], count[index]+dp[i-1][s])
@@ -51,25 +49,25 @@ for _ in 0..<T {
     
 }
 
-func blocked(_ arr:[String], _ x: Int) -> Bool {
-    for i in 0..<arr.count {
-        if arr[i] == "x"  {
-            if x & (1<<i) >= 1 {
-                return false
-            }
-            
-        }
-    }
-    return true
-//    var answer = 0
-//    var temp = 1
+func blocked(_ arr:[String]) -> Int {
 //    for i in 0..<arr.count {
-//        if arr[i] == "x" {
-//            answer += temp
+//        if arr[i] == "x"  {
+//            if x & (1<<i) >= 1 {
+//                return false
+//            }
+//
 //        }
-//        temp = temp * 2
 //    }
-//    return answer
+//    return true
+    var answer = 0
+    var temp = 1
+    for i in 0..<arr.count {
+        if arr[i] == "x" {
+            answer += temp
+        }
+        temp = temp * 2
+    }
+    return answer
 }
 
 func adj(_ x : Int) -> Bool {
@@ -98,7 +96,6 @@ func can(_ before: Int, _ after: Int) -> Bool {
     return true
 }
 
-
 func counting(_ x:Int) -> Int {
     var s = x
     var answer = 0
@@ -112,65 +109,4 @@ func counting(_ x:Int) -> Int {
         }
     }
     return answer
-}
-
-
-
-
-final class FileIO {
-    private let buffer:[UInt8]
-    private var index: Int = 0
-
-    init(fileHandle: FileHandle = FileHandle.standardInput) {
-        
-        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
-    }
-
-    @inline(__always) private func read() -> UInt8 {
-        defer { index += 1 }
-
-        return buffer[index]
-    }
-
-    @inline(__always) func readInt() -> Int {
-        var sum = 0
-        var now = read()
-        var isPositive = true
-
-        while now == 10
-                || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
-        while now >= 48, now <= 57 {
-            sum = sum * 10 + Int(now-48)
-            now = read()
-        }
-
-        return sum * (isPositive ? 1:-1)
-    }
-
-    @inline(__always) func readString() -> String {
-        var now = read()
-
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-
-        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
-    }
-
-    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
-        var now = read()
-
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-
-        return Array(buffer[beginIndex..<(index-1)])
-    }
 }
